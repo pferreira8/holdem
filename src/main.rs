086c45a91455b46ff814b772492480676a45c686
pferreira8 <officialphilipferreira@gmail.com>
2023-03-21 08:44:58 -0400

    full-pair distribution feature
    adds about 17ms to default runtime

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 
 use std::time::Instant;
 use rand::Rng;
+use std::fmt;
+
 //TODO IMPLEMENTATION
 // #[derive(Debug, PartialEq)]
 // enum HandRank {
@@ -29,12 +31,8 @@ struct Args {
     // hand_lookup: String,
     // target_hand: String,
 }
-// struct TargetHand {
-//     aces: String,
-//     kings: String, 
-    
-// }
 
+//DEPRACTING PROBABLY
 // impl TargetHand {
 //     fn new(target: &str) {
 //         match target {
@@ -75,8 +73,60 @@ fn track_runtime(pass_start: Instant) {
 
     println!("Time elapsed: {:?}", duration);
 }
-
+// logic to integrate the MonteCarloPairDistribution struct
+// with the simulator
+fn track_simulation_pairs(mut pair_distribution: MonteCarloPairDistribution, h: Hand) -> MonteCarloPairDistribution {
+    // let mut pair_distribution = MonteCarloPairDistribution::default();
+    match (h.cards[0].rank, h.cards[1].rank) {
+        (Rank::Two, Rank::Two) => pair_distribution.deuces +=1,
+        (Rank::Three, Rank::Three) => pair_distribution.threes +=1,
+        (Rank::Four, Rank::Four) => pair_distribution.fours +=1,
+        (Rank::Five, Rank::Five) => pair_distribution.fives +=1,
+        (Rank::Six, Rank::Six) => pair_distribution.sixes +=1,
+        (Rank::Seven, Rank::Seven) => pair_distribution.sevens +=1,
+        (Rank::Eight, Rank::Eight) => pair_distribution.eights +=1,
+        (Rank::Nine, Rank::Nine) => pair_distribution.nines +=1,
+        (Rank::Ten, Rank::Ten) => pair_distribution.tens +=1,
+        (Rank::Jack, Rank::Jack) => pair_distribution.jacks +=1,
+        (Rank::Queen, Rank::Queen) => pair_distribution.queens +=1,
+        (Rank::King, Rank::King) => pair_distribution.kings +=1,
+        (Rank::Ace, Rank::Ace) => pair_distribution.aces +=1,
+        _ => {} //skip anything non-pair hand combos
+        
+    }
+    //update pair distribution tracker and return it
+    pair_distribution
+    
+}
 //STRUCT AND IMPL LOGIC
+#[derive(Default)]
+//Default value of usize is 0 
+struct MonteCarloPairDistribution {
+    total_hands: u32,
+    deuces: usize,
+    threes: usize, 
+    fours: usize,
+    fives: usize,
+    sixes: usize,
+    sevens: usize,
+    eights: usize,
+    nines: usize,
+    tens: usize,
+    jacks: usize,
+    queens: usize,
+    kings: usize,
+    aces: usize,
+}
+impl fmt::Display for MonteCarloPairDistribution {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(
+            f,
+            "2️⃣: {}/{}\n3️⃣: {}/{}\n4️⃣: {}/{}\n5️⃣: {}/{}\n6️⃣: {}/{}\n7️⃣: {}/{}\n8️⃣: {}/{}\n9️⃣: {}/{}\n🔟: {}/{}\n👑: {}/{}\n👸: {}/{}\n🃏: {}/{}\n🅰️: {}/{}",
+            self.deuces, self.total_hands, self.threes, self.total_hands, self.fours, self.total_hands, self.fives, self.total_hands, self.sixes, self.total_hands, self.sevens, self.total_hands, self.eights, self.total_hands, self.nines, self.total_hands, self.tens, self.total_hands, self.kings, self.total_hands, self.queens, self.total_hands, self.jacks, self.total_hands, self.aces, self.total_hands
+        )
+    }
+}
+
 #[derive(Debug, PartialEq, Clone, Parser)]
 enum Suit {
     Hearts,
@@ -195,20 +245,22 @@ impl Hand {
         self.cards[0].rank == self.cards[1].rank
     }
 
-    fn suited_hand(&self) -> bool {
-        self.cards[0].suit == self.cards[1].suit
-    }
-    fn got_aces(&self) -> bool {
-        self.cards[0].rank.eq(&Rank::Ace) && self.cards[1].rank.eq(&Rank::Ace)
-    }
+    // fn suited_hand(&self) -> bool {
+    //     self.cards[0].suit == self.cards[1].suit
+    // }
+    // fn got_aces(&self) -> bool {
+    //     self.cards[0].rank.eq(&Rank::Ace) && self.cards[1].rank.eq(&Rank::Ace)
+    // }
 }
 
-fn simulation_builder(n_sims: u32)  {
+fn simulation_builder(n_sims: u32) {
+    let mut pair_tracker = MonteCarloPairDistribution::default();
+    pair_tracker.total_hands = n_sims;
     let mut deck = Box::new(Deck::new());
     deck.shuffle();
-    let mut pair_tracker = 0;
-    let mut suit_tracker = 0;
-    let mut rockets = 0;
+    // let mut pair_tracker = 0;
+    // let mut suit_tracker = 0;
+    // let mut rockets = 0;
     for _ in 0..n_sims {    
         let rng_hand = deck.deal(2).map(|cards| Hand::new(cards));
         // error handler to use a new deck
@@ -218,34 +270,39 @@ fn simulation_builder(n_sims: u32)  {
 
         }
         match rng_hand {
-            Some(Ok(hand)) => {
-                // println!("{:?}", hand.cards);
-                if hand.paired_hand() {
-                    // println!("Paired hand");
-                    pair_tracker+=1;
-                } 
-                if hand.suited_hand() {
-                    suit_tracker+=1;
-                    // println!("Suited hand");
-                }
-                if hand.got_aces() {
-                    rockets+=1;
-                }
+            Some(Ok(sim_hand)) => {
+                pair_tracker = track_simulation_pairs(pair_tracker, sim_hand);
+                
+                // if sim_hand.paired_hand() {
+                //     // println!("Paired hand");
+                //     pair_tracker+=1;
+                // } 
+                // if sim_hand.suited_hand() {
+                //     suit_tracker+=1;
+                //     // println!("Suited hand");
+                // }
+                // if sim_hand.got_aces() {
+                //     rockets+=1;
+                // }
+                
             },
             _ => println!("No hand dealt")
         }
-        // TODO ADD EVALUATION LOGIC 
-        // after running x sims, show stats at that point, pause 5 seconds
+    } // END FOR LOOP
+    
+    //RETURN PAIR TRACKER STRUCT
+    println!("randomized hands dealt: \n {}", n_sims);
+    println!("pair distribution \n {}",pair_tracker);
+    
+    
+    // OLD STATISTICS SIMULATION 
+    // println!("Out of {} iterations: \n{} had a matching pair,\n{} were suited preflop \n", 
+    //     n_sims, 
+    //     pair_tracker, 
+    //     suit_tracker);
         
-    // END 
-    } // FOR LOOP
-    println!("Out of {} iterations: \n{} had a matching pair,\n{} were suited preflop \n", 
-        n_sims, 
-        pair_tracker, 
-        suit_tracker);
-        
-    println!("POCKET ROCKET COUNTER: {:?}", rockets);
-    println!("sample probability of aces: {:?}%", rockets as f32 / n_sims as f32);
-    println!("sample probability of any pair: {:?}%", pair_tracker as f32 / n_sims as f32);
-    println!("sample probability of any suited hand: {:?}%\n\n", suit_tracker as f32 / n_sims as f32);
+    // println!("POCKET ROCKET COUNTER: {:?}", rockets);
+    // println!("sample probability of aces: {:?}%", rockets as f32 / n_sims as f32);
+    // println!("sample probability of any pair: {:?}%", pair_tracker as f32 / n_sims as f32);
+    // println!("sample probability of any suited hand: {:?}%\n\n", suit_tracker as f32 / n_sims as f32);
 }
